d106bd91125cffd243b196566c19b653dbd11a65
Glitch (lily-royal-steed) <none>
2022-01-05 23:20:35 +0000

    🪠✒️ Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,29 +1,48 @@
-$(".play").click(function() {
+$(".play").click(function () {
   // this is the range of 0 to 2.999
   // we want three chunks between three answers
   // 3 / 3 === 1
   // 0 -> 1 rock
   // 1 -> 2 paper
   // 2 -> 3 scissors
+
+  // Set up variables for later
   let randomNumber = Math.random() * 3;
   let choice = $(".input").val();
+
+  // Set some info on the page
   $(".userChoice").text(choice);
-  
+  $(".result").text("");
+
+  // Get the computer choice
   let computerChoice = "No choice.";
-  
   if (randomNumber <= 1) {
     computerChoice = "Rock";
-  } else if (randomNumber > 1 
-             && randomNumber <= 2) {
+  } else if (randomNumber > 1 && randomNumber <= 2) {
     computerChoice = "Paper";
   } else {
     computerChoice = "Scissors";
   }
-  
-  // once we have the word we care about
+
+  // Set computer choice once we have the word we care about
   $(".computerChoice").text(computerChoice);
+
+  // Find the winner
+  let rockWon = choice === "Rock" && computerChoice === "Scissors";
+  let paperWon = choice === "Paper" && computerChoice === "Rock";
+  let scissorsWon = choice === "Scissors" && computerChoice === "Paper";
+  let userWon = rockWon || paperWon || scissorsWon;
   
+  // Get the result
+  let result = "";
   if (choice === computerChoice) {
-    $(".result").text("tie");
+    result = "Tie.";
+  } else if (userWon) {
+    result = "You win.";
+  } else {
+    result = "You lose. Try again.";
   }
+
+  // Set the result
+  $(".result").text(result);
 });
